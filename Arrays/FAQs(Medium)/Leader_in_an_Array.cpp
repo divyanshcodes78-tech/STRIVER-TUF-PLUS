6868842df51a49705d89c776904d67c0c4ad0bc2
DIVYANSH SINGH <250735262+divyanshcodes78-tech@users.noreply.github.com>
2026-06-10 11:13:28 +0530

    Leader_in_an_Array

--- Arrays/FAQs(Medium)/Leader_in_an_Array.cpp
+++ Arrays/FAQs(Medium)/Leader_in_an_Array.cpp
@@ -1,9 +1,60 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-
-int main() 
+void leaders2(vector<int>& nums) 
 {
-    cout << "hello world" << endl;
+    // Brute Force Approach:
+    
+    vector<int> result;
+
+    int largestEle = nums.back();
+    result.push_back(largestEle);
+
+    for(int i = nums.size() - 2; i >= 0; i--)
+    {
+        if(nums[i] > largestEle)
+        {
+            largestEle = nums[i];
+            result.push_back(largestEle);
+        }
+    }
+
+    reverse(result.begin(), result.end());
+
+for(int x : result)
+{
+    cout << x << " ";
+}
+}
+
+vector<int> leaders1(vector<int>& nums) 
+{
+    // Brute Force Approach:
+
+    vector<int> result;
+
+    for(int i = 0; i < nums.size(); i++)
+    {
+        bool isLeader = true;
+
+        for(int j = (i + 1); j < nums.size(); j++)
+        {
+            if(nums[j] >= nums[i])
+            {
+                isLeader = false;
+                break;
+            }
+        }
+
+        if(isLeader)
+        {
+            result.push_back(nums[i]);
+        }
+    }
+}
+int main() 
+{vector<int> nums = {1, 2, 5, 3, 1, 2};
+    leaders2(nums);
     return 0;
 }
